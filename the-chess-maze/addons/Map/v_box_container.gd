65cd2d0f32d74f41d967242e19e5d89385ce43bc
briisc61 <110065537+briisc61@users.noreply.github.com>
2025-06-19 10:59:27 +1200

    finish menu

--- the-chess-maze/addons/Map/v_box_container.gd
+++ the-chess-maze/addons/Map/v_box_container.gd
@@ -7,7 +7,8 @@ func _on_button_new_game_pressed() -> void:
 	get_tree().change_scene_to_packed(TEMPLATE_MAP_SCENE)
 
 
-
-
 func _on_button_3_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_button_2_load_pressed() -> void:
+	get_tree().change_scene_to_packed(TEMPLATE_MAP_SCENE)
